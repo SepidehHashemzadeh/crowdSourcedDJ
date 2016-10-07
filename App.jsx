4ea1029edc37883b38d1e1959634833abaeb49b9
abjsaha <abhijoysaha25@gmail.com>
2016-10-07 12:22:12 -0700

    Added background to home page

--- App.jsx
+++ App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+	
+	render() {
+		var imgUrl = './resources/images/background.png';
+		var divStyle = {
+  			backgroundColor: 'black',
+  			width: '100vh',
+  			height: '100vh'
+		};
+	    return (
+	    	<div style={divStyle}>
+	    		Hello World!
+	    	</div>
+	    );
+	}
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,13 @@ var config = {
             query: {
                presets: ['es2015', 'react']
             }
+         },
+         {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+               'file?hash=sha512&digest=hex&name=[hash].[ext]',
+               'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
          }
       ]
    }
