03e2785d0516228fa6ff83fdc5a98de6874aac76
abjsaha <abhijoysaha25@gmail.com>
2016-10-08 20:19:51 -0700

    Added Homepage Sign Up and Sign In Buttons

--- App.jsx
+++ App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-
+import s from './resources/css/homePage.css';
+require("./resources/css/homePage.css");
 class App extends React.Component {
 	
 	render() {
 		var imgUrl = './resources/images/background.png';
-		var divStyle = {
-  			backgroundColor: 'black',
-  			width: '100vw',
-  			height: '100vh'
-		};
 	    return (
-	    	<div style={divStyle}>
-	    		Hello World!
+	    	<div className="divStyle">
+	    		<div className="innerStyle">
+	    			<div className="inner">Welcome!</div>
+	    			<div className="inner">*LOGO*</div>
+	    			<div className="inner">
+		    			<div className="inner signIn signUpIn">Sign In</div>
+		    			<div className="inner signUp signUpIn">Sign Up</div>
+	    			</div>
+	    		</div>
 	    	</div>
 	    );
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ var config = {
                'file?hash=sha512&digest=hex&name=[hash].[ext]',
                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
             ]
+         },
+         { 
+            test: /\.css$/, loader: "style-loader!css-loader" 
+         },
+         { 
+            test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, 
+            loader: 'file-loader?name=fonts/[name].[ext]' 
          }
       ]
    }
