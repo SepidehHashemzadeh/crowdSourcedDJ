128e15d9e8aa0e8967cf5a5bf7b1f31c221eb85a
medoelling <medoelling@gmail.com>
2016-10-28 01:02:13 -0700

    Create fields for event form, add css file for event form

--- App.jsx
+++ App.jsx
@@ -3,21 +3,22 @@ import FBLogin from './components/FBLogin.jsx'
 import CreateEventForm from './components/CreateEventForm.jsx'
 require("./resources/css/homePage.css");
 class App extends React.Component {
-
 	render() {
 		var imgUrl = require('./resources/images/logov4.png');
 	    return (
-	    	<div className="homePageDiv">
-	    		<div className="homePageColumn">
-	    			<div className="homePageRow" id="homePageWelcome">dj.que</div>
-	    			<div className="homePageRow"><img src={imgUrl} id="logoHomePage"></img></div>
-	    			{/*<div className="homePageRow" id="signBtnDiv">
-		    			<div className="homePageRow signInBtn hvr-back-pulse signUpInBtn">Sign In</div>
-		    			<div className="homePageRow signUpBtn hvr-back-pulse signUpInBtn">Sign Up</div>
-	    			</div>*/}
-	    			<div className="homePageRow" id="signBtnDiv"><FBLogin/></div>
-	    			<CreateEventForm/>
-	    		</div>
+	    	<div>
+		    	<div className="homePageDiv">
+		    		<div className="homePageColumn">
+		    			<div className="homePageRow" id="homePageWelcome">dj.que</div>
+		    			<div className="homePageRow"><img src={imgUrl} id="logoHomePage"></img></div>
+		    			{/*<div className="homePageRow" id="signBtnDiv">
+			    			<div className="homePageRow signInBtn hvr-back-pulse signUpInBtn">Sign In</div>
+			    			<div className="homePageRow signUpBtn hvr-back-pulse signUpInBtn">Sign Up</div>
+		    			</div>*/}
+		    			<div className="homePageRow" id="signBtnDiv"><FBLogin/></div>
+		    		</div>
+		    	</div>
+	    		<CreateEventForm/>
 	    	</div>
 	    );
 	}
--- components/CreateEventForm.jsx
+++ components/CreateEventForm.jsx
@@ -1,15 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-require("./../resources/css/homePage.css");
+//require("./../resources/css/homePage.css");
+require("./../resources/css/createEventForm.css");
+var CreateEventForm = React.createClass({
+	getInitialState: function() {
+		return { 
+			eventName: '',
+			eventLocation: '',
+			eventStartTime: '',
+			eventDescription: ''
+		};
+	},
 
-class CreateEventForm extends React.Component {
-	render() {
+	handleEventName: function (e){
+		this.setState({ eventName: e.target.value});
+	},
+	handleEventLocation: function (e){
+		this.setState({ eventLocation: e.target.value});
+	},
+	handleEventStartTime: function(e){
+		this.setState({ eventStartTime: e.target.value});
+	},
+	handleEventDescription: function(e){
+		this.setState({ eventDescription: e.target.value});
+	},
+
+
+	render: function(){
 	    return (
 	    	<div className="createEventFormDiv">
-	    		<h1>This is the form to create an event!!</h1>
+	    		<h3>Use this form to create your event!!! cool!!!</h3>
+		    		<h5>Event Name:</h5>
+		    		<input type="text"
+
+		    			   onChange={this.handleEventName}
+		    		       value={this.state.eventName} />
+		    		<h5>Your event name is: {this.state.eventName} </h5>
+
+	    		<h5>Start Time:</h5>
+	    		<input type="datetime-local"
+	    			   onChange={this.handleEventStartTime}
+	    		       value={this.state.eventStartTime} />
+	    		<h5>Your event time is: {this.state.eventStartTime} </h5>
+
+	    		<h5>Location:</h5>
+	    		<input type="text"
+	    			   onChange={this.handleEventLocation}
+	    		       value={this.state.eventLocation} />
+	    		<h5>Your event location is: {this.state.eventLocation} </h5>
+
+				<h5>Description:</h5>
+	    		<textarea
+	    			   rows="4"
+	    			   cols="40"
+	    			   onChange={this.handleEventDescription}
+	    		       value={this.state.eventDescription} />
+	    		<h5>Your event description is: {this.state.eventDescription} </h5>
+	    		
 	    	</div>
 	    );
 	}
-}
+});
 
 export default CreateEventForm;
