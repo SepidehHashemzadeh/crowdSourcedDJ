3cb5177de07bbac2334d8ea4a56ad36413efed6c
medoelling <medoelling@gmail.com>
2016-10-29 11:44:58 -0700

    styling of create event form using template.

--- components/CreateEventForm.jsx
+++ components/CreateEventForm.jsx
@@ -28,50 +28,80 @@ var CreateEventForm = React.createClass({
 	render: function(){
 	    return (
 	    	<div className="createEventFormDiv">
-		    		<h3>Use this form to create your event!!! cool!!!</h3>
-		    		<div className="createEventEntry">
-			    		<h5 className="createEventFieldName">Event Name:</h5>
-			    		<input type="text"
-			    		       className="createEventFieldInput"
-			    			   onChange={this.handleEventName}
-			    		       value={this.state.eventName} />
-			    	</div>
-
-			    	<div className="createEventEntry">
-			    		<h5 className="createEventFieldName">Start Time:</h5>
-			    		<input type="datetime-local"
-			    		       className="createEventFieldInput"
-			    			   onChange={this.handleEventStartTime}
-			    		       value={this.state.eventStartTime} />
-			    	</div>
-
-			    	<div className="createEventEntry">
-			    		<h5 className="createEventFieldName">Location:</h5>
-			    		<input type="text"
-			    		       className="createEventFieldInput"
-			    			   onChange={this.handleEventLocation}
-			    		       value={this.state.eventLocation} />
-			    	</div>
-
-			    	<div className="createEventEntry">
-						<h5 className="createEventFieldName">Description:</h5>
-			    		<textarea
-			    		       className="createEventFieldInput"
-			    			   rows="4"
-			    			   cols="40"
+		    	<form action="#">
+				  <header>
+				    <h2 className="formTitle">Create New Event</h2>
+				    <div>add info to create event then u ken be partee leader</div>
+				  </header>
+				  
+				  <div>
+				    <label class="desc" id="title1" for="Field1">Event Name</label>
+				    <div>
+				      <input id="Field1" name="Field1" type="text" class="field text fn"size="8" tabindex="1"
+				             className="createEventFieldInput"
+			    			 onChange={this.handleEventName}
+				             value={this.state.eventName}/>
+				    </div>
+				  </div>
+				    
+				  <div>
+				    <label class="desc" id="title3" for="Field3">
+				      Location
+				    </label>
+				    <div>
+				      <input id="Field3" name="Field3" type="text" spellcheck="false" maxlength="255" tabindex="3"
+				             className="createEventFieldInput"
+			    			 onChange={this.handleEventLocation}
+			    		     value={this.state.eventLocation}/> 
+				   </div>
+				  </div>
+				    
+				  <div>
+				    <label class="desc" id="title4" for="Field4">
+				      Event Description
+				    </label>
+				  
+				    <div>
+				      <textarea id="Field4" name="Field4" spellcheck="true" rows="5" cols="45" tabindex="4"
+				               className="createEventFieldInput"
 			    			   onChange={this.handleEventDescription}
-			    		       value={this.state.eventDescription} />
-			    	</div>
+			    		       value={this.state.eventDescription}></textarea>
+				    </div>
+				  </div>
+				  
+				  
+				  <div>
+				    <label class="desc" id="title106" for="Field106">
+				    	Event Time
+				    </label>
+				    <div>
+				    <input type="datetime-local" id="Field106" name="Field106" class="field select medium" tabindex="11"
+			    		    className="createEventFieldInput"
+			         	    onChange={this.handleEventStartTime}
+			    		    value={this.state.eventStartTime} />
+				    </div>
+				  </div>
+				  
+				  <div>
+						<div>
+				  		<input id="saveForm" name="saveForm" type="submit" value="Submit"/>
+				    </div>
+					</div>
 
-			    	<div className="test">
+					<div className="test">
 			    		<h5>Your event name is: {this.state.eventName} </h5>
 			    		<h5>Your event time is: {this.state.eventStartTime} </h5>
 			    		<h5>Your event location is: {this.state.eventLocation} </h5>
 			    		<h5>Your event description is: {this.state.eventDescription} </h5>
 			    	</div>
-	    	</div>
+				  
+				</form>
+			</div>
 	    );
 	}
 });
 
-export default CreateEventForm;
+
+
+export default CreateEventForm; 
+
