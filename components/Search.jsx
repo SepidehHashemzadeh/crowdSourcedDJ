8949be32f10e50053eaf72546dc3f295492436e9
killianjackson <killianjackson@yahoo.com>
2016-11-02 18:45:29 -0700

    Added search.css file

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-//require("./resources/css/search.css");
+require("../resources/css/search.css");
 class Search extends React.Component {
       render() {
                return (
-               //<div class="box">
-               <div class="container-1">
+               <div className="container-1">
                <span class="icon"><i class="fa fa-search"></i></span>
                <input type="search" id="search" placeholder="Search existing events" />
                </div>
-               //</div>
                );
         }
 }
