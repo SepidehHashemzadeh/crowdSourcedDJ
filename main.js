e79802a5efa54e999a35b5183d814f629398a68c
Adam Jones <adam.jones@ucla.edu>
2016-11-02 21:55:35 -0700

    Setup

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import EventPage from './components/EventPage.jsx';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<EventPage />, document.getElementById('eventPage'));
