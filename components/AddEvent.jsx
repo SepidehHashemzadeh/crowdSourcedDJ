48f108b4a9a8bea63ee58e27e90942564cdd1395
killianjackson <killianjackson@yahoo.com>
2016-11-02 19:21:54 -0700

    Styled search bar in search.css. Added addEvent.css file

--- components/AddEvent.jsx
+++ components/AddEvent.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 class AddEvent extends React.Component {
-      render() {
-               return (
-			<button type="button" onClick={this.addEventClick}>Add Event</button>
-	              );
-      }
+	render() {
+        return (
+			<button className="addEvent" type="button" onClick={this.addEventClick}>Add Event</button>
+	    );
+    }
 
-      addEventClick() {
-        alert("Going to add event page!");
-      }
+    addEventClick() {
+    	alert("Going to add event page!");
+    }
 }
 
 export default AddEvent;
--- components/Search.jsx
+++ components/Search.jsx
@@ -3,10 +3,12 @@ require("../resources/css/search.css");
 class Search extends React.Component {
       render() {
                return (
-               <div className="container-1">
-               <span class="icon"><i class="fa fa-search"></i></span>
-               <input type="search" id="search" placeholder="Search existing events" />
-               </div>
+               	<div className="box">
+                 <div className="container-1">
+                   <span className="icon"><i className="fa fa-search"></i></span>
+                   <input type="search" id="search" placeholder="Search existing events" />
+                 </div>
+                 </div>
                );
         }
 }
