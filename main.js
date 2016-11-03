968a3996fec183ac4ebd0caa39a2c7f3ee88578f
killianjackson <killianjackson@yahoo.com>
2016-11-02 18:28:26 -0700

    Moved AddEvent and Search Event classes to components directory

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import Search from './Search.jsx';
-import AddEvent from './AddEvent.jsx';
+import Search from './components/Search.jsx';
+import AddEvent from './components/AddEvent.jsx';
 
 ReactDOM.render(<AddEvent />, document.getElementById('add'));
 ReactDOM.render(<Search />, document.getElementById('search'));
