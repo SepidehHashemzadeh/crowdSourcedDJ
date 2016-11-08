776faeb7437e65a658810835ae0670ce59ee3550
medoelling <medoelling@gmail.com>
2016-11-08 01:00:46 -0800

    added back to dashboard and end event functionality with buttons

--- components/EventPageLeader.jsx
+++ components/EventPageLeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Input } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Input, ButtonToolbar } from 'reactstrap';
 import YouTubePlayer from 'react-youtube-player';
 require("./../resources/css/eventPage.css");
 
@@ -13,12 +13,20 @@ class EventPageLeader extends React.Component {
 			eventStartTime: "",
 			eventDescription: "",
 			eventID: 22,
+			eventIsEnded: false,
 			songID: "",
-			queue: []
+			queue: [],
+			hide: false
 		};
 		this.render = this.render.bind(this);
+		this.back = this.back.bind(this);
+		this.end = this.end.bind(this);
+		this.edit = this.edit.bind(this);
 	}
 	componentWillMount() {
+		this.setState({
+			hide: false
+		});
 		var url = "https://djque.herokuapp.com/?query="; 
 
 		var eventQuery = "SELECT * FROM Events WHERE id="+ this.state.eventID + ";";
@@ -30,7 +38,8 @@ class EventPageLeader extends React.Component {
 				eventName: res[0].name,
 				eventLocation: res[0].location,
 				eventStartTime: res[0].startTime,
-				eventDescription: res[0].description
+				eventDescription: res[0].description,
+				eventIsEnded: res[0].isEnded
 			});
 		});
 
@@ -55,31 +64,63 @@ class EventPageLeader extends React.Component {
 		var date = this.state.eventStartTime.substring(8,9);
 		console.log(year);
 	}*/
+	back(){
+		console.log("back");
+		this.setState({
+			hide: true
+		});
+	}
+	end(){
+		var url = "https://djque.herokuapp.com/?query="; 
+		var endEventQuery = "UPDATE Events SET isEnded=true WHERE id="+this.state.eventID+";";
+		console.log(endEventQuery);
+		console.log(encodeURI(url + endEventQuery));
+		fetch(encodeURI(url + endEventQuery)).then((res) => {
+			return res.json();
+		}).then((res) => {
+			this.setState({
+				isEnded: true
+			});
+		});
+	}
+	edit(){
+	}
 	render() {
 		return (
-			<div id="eventPageLeader">
-				<h2 className="eventName">{this.state.eventName}</h2>
-				<p className="eventLocationTime">{this.state.eventLocation} at {this.state.eventStartTime}</p>
-				<p>{this.state.eventDescription}</p>
-				<hr/>
-				<div id="addSong">
-					<p>Add Song to Queue Here!</p>
+			<div>
+			{ this.state.hide ? null : 
+				<div id="eventPageLeader">
+					<div id="eventPageLeaderHeader">
+						<h2 className="eventName">{this.state.eventName}</h2>
+						<ButtonToolbar>
+							<Button color="default" onClick={this.back}>Back</Button>
+							<Button color="danger" onClick={this.end}>End</Button>
+							<Button color="info" onClick={this.edit}>Edit</Button>
+						</ButtonToolbar>
+					</div>
+					<p className="eventLocationTime">{this.state.eventLocation} at {this.state.eventStartTime}</p>
+					<p>{this.state.eventDescription}</p>
+					<hr/>
+					<div id="addSong">
+						<p>Add Song to Queue Here!</p>
+					</div>
+					<hr/>
+					<div id="queue">
+						<p>Display Widgets Here!</p>
+						{ 	this.state.queue.map(function(vidID, i) {
+								return 	<div key={i}>
+											<YouTubePlayer
+								            	height='150'
+								            	playbackState='paused'
+								            	videoId={vidID}
+								            	width='320'
+								        	/> <br/>
+								        </div>
+					       	})
+				    	}
+					</div>
 				</div>
-				<hr/>
-				<div id="queue">
-					<p>Display Widgets Here!</p>
-					{ 	this.state.queue.map(function(vidID, i) {
-							return 	<div key={i}>
-										<YouTubePlayer
-							            	height='150'
-							            	playbackState='paused'
-							            	videoId={vidID}
-							            	width='320'
-							        	/> <br/>
-							        </div>
-				       	})
-			    	}
-				</div>
+			}
 			</div>
 		);
 	}
