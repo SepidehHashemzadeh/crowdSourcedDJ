140071d4e7cde7c1627f13e7c64c9adfd431e41d
Adam Jones <adam.jones@ucla.edu>
2016-11-14 21:39:39 -0800

    Added Jest CSS parsing

--- __tests__/ControlledTabsTest.jsx
+++ __tests__/ControlledTabsTest.jsx
@@ -9,16 +9,4 @@ test('Buttons toggle view correctly', () => {
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseEnter();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseLeave();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
 });
