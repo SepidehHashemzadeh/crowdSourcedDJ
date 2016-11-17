60da5c982b215af403a9decb22550edd93874166
SepidehHashemzadeh <sepidehhz@g.ucla.edu>
2016-11-17 00:00:21 -0800

    Update EventPageLeader.jsx

--- components/EventPageLeader.jsx
+++ components/EventPageLeader.jsx
@@ -257,7 +257,7 @@ class EventPageLeader extends React.Component {
 							{ (this.userIsLeader() && !this.state.eventIsEnded) ?  
 								<div>
 									<Button color="danger" onClick={this.end}>End</Button>
-									<Button color="info" onClick={this.end}>Edit</Button>
+									<Button color="info" onClick={this.edit}>Edit</Button>
 								</div>
 							:
 								null 
