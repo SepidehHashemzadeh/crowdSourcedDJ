912f8a3b0cb3512ba31a005ec4861ca519592e6a
abjsaha <abhijoysaha25@gmail.com>
2016-11-19 00:39:19 -0800

    Cleaned up code and made the crashing problem stop

--- components/EventListItem.jsx
+++ components/EventListItem.jsx
@@ -11,16 +11,17 @@ class EventListItem extends React.Component {
 			pClass: "pNotHovered",
 			divClass: "divNotHovered"
 		};
+		this.props.userInfo.then((response) => {
+			this.setState({ userInfo: response[0] });
+		});
 		this.getEventDiv = this.getEventDiv.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 		this.render = this.render.bind(this);
 		this.handleOnMouseEnter = this.handleOnMouseEnter.bind(this);
 		this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this);
 	}
-	componentWillMount() {
-		this.props.userInfo.then((response) => {
-			this.setState({ userInfo: response[0] });
-		});
+	componentDidMount() {
+		
 	}
 	handleOnMouseEnter() {
 		this.setState({
