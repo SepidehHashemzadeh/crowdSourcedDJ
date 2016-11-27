c7502809d7a2e2bdb79e99dba7e0d6b475aed0d3
SepidehHashemzadeh <sepidehhz@g.ucla.edu>
2016-11-27 00:14:26 -0800

    Update ControlledTabs.spec.js

--- tests/ControlledTabs.spec.js
+++ tests/ControlledTabs.spec.js
@@ -13,23 +13,16 @@ it('it should render 2 label component', () => {
     const wrapper = shallow(<ControlledTabs/>);
     expect(wrapper.find("label")).to.have.length(2);
   });
-  
-
-});
-    
-    
+}); 
  describe('ControlledTabs behavior ', () => {
   let select1;
   let handleSelect;
- 
    const spy = sinon.spy()
-    const html= shallow(<ControlledTabs handleSelect={spy} />);
-                        
+    const html= shallow(<ControlledTabs handleSelect={spy} />);     
     it('test ControlledTabs behavior', () => {
     html.setState({ key: 1});
     const modal = html.find('.c-tabs').first()
     modal.simulate('Change')
     expect(spy.notCalled).to.be.true
-
   });
 });
